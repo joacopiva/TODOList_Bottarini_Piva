f46d926e89d11b0eb6570a711f5cfcb8216d4301
JOAQUIN PIVA <47870976@ort>
2024-04-04 11:57:48 -0300

    creando las checkbox

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 let ArrayTareas = []
+let Tarea = 
+{
+    nuevaTarea: null,
+    fechaHoraCreado: null,
+    fechaHoraTachado: null,
+};
+
+const CrearTarea = () =>
+{    
+    let nuevatarea = document.getElementById("NewTask").value;
+
+    principal(nuevatarea)
+}  
+
 
 const principal = (nuevatarea) =>
 {   
     let tareas = document.getElementById("Tareas")
 
-    ArrayTareas.push(nuevatarea)
+    Tarea = 
+    {
+        nuevaTarea: nuevatarea,
+        fechaHoraCreado: new Date().toLocaleString(),
+        fechaHoraTachado: new Date().toLocaleString()
+    }
+
+    ArrayTareas.push(Tarea)
 
     tareas.innerHTML = "";
     
@@ -15,8 +36,10 @@ const principal = (nuevatarea) =>
     for(let i = 0; i < ArrayTareas.length; i++)
     {
         let task = document.createElement("p")
-        task.innerHTML = ArrayTareas[i]
-        tareas.appendChild(task)
+        let check = document.createElement("label")
+        task.innerHTML = ArrayTareas[i].nuevaTarea;
+        task.onclick(tachar(task));
+        tareas.appendChild(task);
       
     }
 
@@ -24,16 +47,14 @@ const principal = (nuevatarea) =>
 
 }
 
+function tachar(task)
+{
+    task.style.text-decoration = "line-through";
+}
 
 
 
 
-const CrearTarea = () =>
-{    
-    let nuevatarea = document.getElementById("NewTask").value;
-
-    principal(nuevatarea)
-}  
 
 
 
